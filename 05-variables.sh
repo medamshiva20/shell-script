8517336bfb72723b8f428c95cb97f405dbfd99dd
medamshiva20 <medamshiva20@gmail.com>
2023-10-23 15:15:28 +0530

    username and password fields testing

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Please enter your username"
+read USERNAME #This is  variable where the value of the one user entered
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+read PASSWORD
+echo "Password entered is: $PASSWORD"
