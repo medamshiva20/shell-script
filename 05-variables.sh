a5de544e64d462c1c5fc49b2f30435bdd77c8304
medamshiva20 <medamshiva20@gmail.com>
2023-10-23 15:16:48 +0530

    username and password not showing to user by using -s option

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Please enter your username"
-read USERNAME #This is  variable where the value of the one user entered
+read -s USERNAME #This is  variable where the value of the one user entered
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
-read PASSWORD
+read -s PASSWORD
 echo "Password entered is: $PASSWORD"
