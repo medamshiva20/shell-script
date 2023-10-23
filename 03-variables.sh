77d5ec1392e1e26ce73fd24c70d5731665c73ae6
medamshiva20 <medamshiva20@gmail.com>
2023-10-23 14:14:37 +0530

    we need to pass values from command line

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+##You can always get the values of variables from command line
+
+#!/bin/bash 
+PERSON1=$1 #This is the first value from the command line 
+PERSON2=$2 #This is the second value from the command line 
+
+echo "Hello World, I am practicing Devops with AWS"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
