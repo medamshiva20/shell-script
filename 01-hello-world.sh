053d15ddf1e483455ee0323a45f0bc34be2bb3c8
medamshiva20 <medamshiva20@gmail.com>
2023-10-23 13:18:13 +0530

    Hello world script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash 
 echo "Hello World, I am practicing Devops with AWS"
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, how are you Ramesh"
