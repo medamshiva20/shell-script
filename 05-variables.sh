b3f6be9b92f6915384367b0ecf24e68787c6d8c5
medamshiva20 <medamshiva20@gmail.com>
2023-10-23 15:42:02 +0530

    prompting username and password

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,11 @@
+##We can get the values prompting through read command
+
 #!/bin/bash
 echo "Please enter your username"
 read -s USERNAME #This is  variable where the value of the one user entered
-echo "Username entered is: $USERNAME"
 
+#This is only for validation, we should not print in the terminal
+echo "Username entered is: $USERNAME"
 echo "Please enter your password"
 read -s PASSWORD
 echo "Password entered is: $PASSWORD"
