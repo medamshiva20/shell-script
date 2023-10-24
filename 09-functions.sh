81a0b52365d8b37069ffa87cfc243a5d45f6bf19
medamshiva20 <medamshiva20@gmail.com>
2023-10-24 17:33:20 +0530

    to the logs in proper format

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-#our program goal is to install mysql
-USERID=$(id -u) # it will get the output and store into USERID 'id -u command it will only give user id'
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 #This function should validate the previous command status and inform to user it is success or failure
 VALIDATE(){
     #$1 --> It will receive the argument1
@@ -14,6 +16,8 @@ VALIDATE(){
     fi
 }
 
+#our program goal is to install mysql
+USERID=$(id -u) # it will get the output and store into USERID 'id -u command it will only give user id'
 if [ $USERID -ne 0 ]
 then
    echo "ERROR:: Please run this script with root access"
@@ -22,10 +26,10 @@ else
    echo "INFO: you are root user"
 fi
 
-yum install mysql -y
+yum install mysql -y &&>>LOGFILE
 #It is our responsibility again to check installation is success or not
 VALIDATE $? "Installing MySQL"
 
-yum install postfix -y 
+yum install postfix -y &>>LOGFILE
 
 VALIDATE $? "Installing postfix"
