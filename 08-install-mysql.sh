35449dde3f6fb86236dc53dd19bea38058923157
medamshiva20 <medamshiva20@gmail.com>
2023-10-24 07:49:21 +0530

    added exit status

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -7,10 +7,11 @@
 #/bin/bash
 
 #our program goal is to install mysql
-USERID=$(id -u) # it will get the output and store into USERID
+USERID=$(id -u) # it will get the output and store into USERID 'id -u command it will only user id'
 if [ $USERID -ne 0 ]
 then
   echo "ERROR:: Please run this script with root access"
+  exit 1 # it will stop the script execution further and can't move forward
 else
   echo "INFO: you are root user"
 fi
