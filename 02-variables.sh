77493fc9d7fb6cf2d942c9f7c6f9983072b32353
medamshiva20 <medamshiva20@gmail.com>
2023-12-08 21:53:23 +0530

    variables are modified

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,8 +2,7 @@
 ##How to execute a command inside shell script and take the output(Interview Question)
 
 #!/bin/bash
-#DATE=$(date)
-DATE=$1
+DATE=$(date)
 
 #shell script run the command inside $() and stores the output into variable called DATE
 echo "Hello, this script is executed at timestamp: $DATE"
