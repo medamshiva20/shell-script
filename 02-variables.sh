52a440a2995ec9462c5f9a256426ef72926e5e79
medamshiva20 <medamshiva20@gmail.com>
2023-12-08 21:51:35 +0530

    variables are modified

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,8 @@
 ##How to execute a command inside shell script and take the output(Interview Question)
 
 #!/bin/bash
-DATE=$(date)
+#DATE=$(date)
+DATE=$1
 
 #shell script run the command inside $() and stores the output into variable called DATE
 echo "Hello, this script is executed at timestamp: $DATE"
--- 12-loops.sh
+++ 12-loops.sh
@@ -1,5 +1,14 @@
+# improvements
+# implement log files
+# implement colors
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print yellow color
+# if installed just print package is alredy installed, it should not run install command
+
 #!/bin/bash
 
+#All args are in $@
 for i in $@
 do
  yum install $i -y
