164217bdce4e47444861ab7274e72325d35572bc
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 17:41:51 +0530

    shell script

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,43 +1,25 @@
 #!/bin/bash 
-#our program goal is to install mysql
-DATE=$(date +%F)
-SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
+USERID=$(id -u)
 #This function should validate the previous command status and inform to user it is success or failure
 VALIDATE(){
-    #$1 --> It will receive the argument1
-    if [ $1 -ne 0 ]
-    then
-     echo -e "$2 ... $R FAILURE $N"
-     exit 1
-    else
-     echo -e "$2 ... $G SUCCESS $N"
-    fi
+ #$1 --> It will receive the argument1
+ if [ $1 -ne 0 ]
+  then 
+      echo "Installation is ...FAILURE"
+      exit 1
+  else
+      echo "Installation is ...SUCCESS"
 }
 
-
-USERID=$(id -u) # it will get the output and store into USERID 'id -u command it will only give user id'
 if [ $USERID -ne 0 ]
-then
-   echo "ERROR:: Please run this script with root access"
-   exit 1
-# else
-#    echo "INFO: you are root user"
+  then
+       echo "ERROR:: Please run this script with root access"
+       exit 1
+  else
+       echo "INFO: you are root user"
 fi
 
-yum install mysql -y &>>$LOGFILE
-#It is our responsibility again to check installation is success or not
-VALIDATE $? "Installing MySQL"
+yum install mysql -y 
+VALIDATE $?
 
-yum install postfix -y &>>$LOGFILE
-
-VALIDATE $? "Installing postfix"
-
-yum install maven -y &>>$LOGFILE
-
-VALIDATE $? "Installing Maven"
+yum install postfix -y 
