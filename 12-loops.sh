802412ce33ebadc748e8279676aed6e1d8d42c43
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 21:29:38 +0530

    shell script

--- 12-loops.sh
+++ 12-loops.sh
@@ -8,8 +8,27 @@
 
 #!/bin/bash
 
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME/$DATE.log
+
+R="\e[31m"
+G="\e\[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+     then
+       echo -e "$2...$R FAILURE $N"
+       exit 1
+     else
+       echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
 #All args are in $@
 for i in $@
 do
- yum install $i -y
+ yum install $i -y &>>$LOGFILE
+ VALIDATE $? "Installation of $@"
 done
