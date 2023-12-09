67511bb2c9263b94f19b5c8a7ec6330b34cec7e4
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 19:10:45 +0530

    shell script

--- 09-functions.sh
+++ 09-functions.sh
@@ -3,15 +3,20 @@ USERID=$(id -u)
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
 #This function should validate the previous command status and inform to user it is success or failure
 VALIDATE(){
  #$1 --> It will receive the argument1
  if [ $1 -ne 0 ]
   then 
-      echo "$2 ...FAILURE"
+      echo "$2 ...$R FAILURE $N"
       exit 1
   else
-      echo "$2 ...SUCCESS"
+      echo "$2 ...$G SUCCESS $N"
  fi
 }
 
