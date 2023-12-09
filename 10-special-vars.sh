2d5391c04002feb471d0edd63d056dca178c5e2c
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 18:32:16 +0530

    shell script

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -7,11 +7,3 @@ echo "script name: $0"
 echo "Variable 1 is: $VAR1"
 echo "All variables: $@"
 echo "Number of variables: $#"
-
-
-
-
-# echo "Script Name: $0"
-# echo "Varibale 1 is: $VAR1"
-# echo "All variables: $@"
-# echo "Number of variables: $#"
