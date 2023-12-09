e378d11c9531e262da1c205a34cc0be36f10faae
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 12:52:21 +0530

    shell script

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -1,36 +1,17 @@
-#1.check root user or not 
+#1.check root user or not
 #2.if not root user, exit the program and inform user to run with sudo access
 #3.if root user install mysql
 # root user id -u --> 0
 #non root user id -u -->1000 or above(ex:not zero)
 
-#/bin/bash
+#!/bin/bash 
 
-#our program goal is to install mysql
-USERID=$(id -u) # it will get the output and store into USERID 'id -u command it will only user id'
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
-then
-  echo "ERROR:: Please run this script with root access"
-  exit 1 # it will stop the script execution and can't execute further or remaining script
-else
-  echo "INFO: you are root user"
+ then
+   echo "ERROR:: Please run this script with root access"
+ else
+   echo "INFO: yor are root user"
 fi
 
-yum install mysql -y
-
-if [ $? -ne 0 ] #$? this command will give result like if it 0 means success and if it is 1 means failure
-then
- echo "Installation of mysql is error"
- exit 1
-else
- echo "Installation of mysql is success"
- fi
-
-yum install postfix -y
-
-if [ $? -ne 0 ]
-then
- echo "Installation of postfix is error"
-else
- echo "Installation of postfix is success"
-fi
+yum install git -y
