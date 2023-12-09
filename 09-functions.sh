70469d2c471d7181242d68a8dea656eb2f99e1d5
medamshiva20 <medamshiva20@gmail.com>
2023-12-09 19:14:02 +0530

    shell script

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,9 +4,9 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
-R=\e[31m
-G=\e[32m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 #This function should validate the previous command status and inform to user it is success or failure
 VALIDATE(){
