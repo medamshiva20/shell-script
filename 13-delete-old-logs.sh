746d48cd4d886088f852c25e3828702451feb496
medamshiva20 <medamshiva20@gmail.com>
2023-12-10 22:57:54 +0530

    shell script

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -8,3 +8,9 @@ LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line 
+do
+  echo "Deleting $line" &>>$LOG_FILE
+  rm -rf $line
+done <<< $FILES_TO_DELETE
