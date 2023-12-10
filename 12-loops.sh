34b750bf2c0e1c30e8bd82d2da847b16ea98541e
medamshiva20 <medamshiva20@gmail.com>
2023-12-10 21:16:24 +0530

    shell script

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,23 +1,15 @@
-# improvements
-# implement log files
-# implement colors
-# implement validations
-# implement validations through functions
-# your script should check package is already installed or not, if already installed print yellow color
-# if installed just print package is alredy installed, it should not run install command
-
 #!/bin/bash
-USERID=$(id -u)
+
 DATE=$(date +%F)
 LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-
-
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
 if [ $USERID -ne 0 ];
 then
@@ -25,29 +17,36 @@ then
     exit 1
 fi
 
-#This function should validate the previous command status and inform to user it is success or failure
 VALIDATE(){
- #$1 --> It will receive the argument1
- if [ $1 -ne 0 ]
-  then 
-      echo -e "$2 ...$R FAILURE $N"
-      exit 1
-  else
-      echo -e "$2 ...$G SUCCESS $N"
- fi
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
 }
 
-#All args are in $@
+# all args are in $@
 for i in $@
 do
-  yum list installed $i &>>$LOGFILE
-  
- if [ $? -ne 0 ]
-  then
-    echo "$i is not installed, let's install it"
-    yum install $i -y &>>$LOGFILE
-    VALIDATE $? "$i"
-  else
-     echo "$Y $i is already installed $N"
- fi
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+
+    #yum install $i -y
 done
+
+# improvements
+# implement log files
+# implement colors
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print yellow color
+# if installed just print package is alredy installed, it should not run install command
