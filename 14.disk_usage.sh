18bd33dbc0d234381f5976747b9664c7875f745c
medamshiva20 <medamshiva20@gmail.com>
2023-12-13 15:50:48 +0530

    added disk usage script

--- 14.disk_usage.sh
+++ 14.disk_usage.sh
@@ -9,6 +9,7 @@ LOGFILE_DIRECTORY=/tmp
 DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOG_FILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+message=""
 
 R="\e[31m"
 G="\e[32m"
@@ -19,7 +20,21 @@ Y="\e[33m"
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 
+#IFS= means internal field seperator is space
 while IFS= read line
 do
-  echo "output: $line"
+  # this command will give you usage in number format for comparision
+  usage=$(echo $line | awk {'print $6'} | cut -d % -f1)
+  # this command will give us partition
+  partition=$(df -hT | grep -vE 'tmpfs|Filesystem' |awk {'print $1'})
+  #now you need to check whether it is more than threshold or not
+  if [ $usage -gt $DISK_USAGE_THRESHOLD ]
+   then
+      message+="HIGH DISK USAGE ON $partition: $usage\n"
+  fi
+  #echo "output: $line"
 done <<< $DISK_USAGE
+
+echo -e "message: $message"
+
+
