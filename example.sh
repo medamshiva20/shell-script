6a6b480cffba4f8516ace8a885614b62e3bfc48c
medamshiva20 <medamshiva20@gmail.com>
2023-12-21 16:52:34 +0530

    Interview of shell script

---
+++ example.sh
@@ -0,0 +1,16 @@
+# We have to write shell script we have to say user input Hello World
+# We have to write length of the string 
+# Convert string to upper case 
+
+#!/bin/bash 
+
+variable1=$1
+
+echo "Stirng name is: $variable1"
+
+str="$variable1"
+n=0
+while read -n1 character; do
+    n=$((n+1)); 
+done < <(echo -n "$str")
+echo "Length of the string is : $n "
