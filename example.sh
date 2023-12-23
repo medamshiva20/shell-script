31534ccc30af3686dd7ab27d0978701da86cf7f5
medamshiva20 <medamshiva20@gmail.com>
2023-12-23 22:58:20 +0530

    updated one

--- example.sh
+++ example.sh
@@ -9,10 +9,3 @@ variable1=$1
 echo "String name is: $variable1"
 echo "Stirng count is: ${#variable1}"
 echo "$variable1" | awk '{print toupper ($0)}'
-
-# str="$variable1"
-# n=0
-# while read -n1 character; do
-#     n=$((n+1)); 
-# done << (echo -n "$str")
-# echo "Length of the string is : $n "
