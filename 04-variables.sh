907a8e8ab0e028eb2764de02dc594437886f289c
medamshiva20 <medamshiva20@gmail.com>
2024-01-18 20:59:03 +0530

    Addition of two numbers

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 #This is the comment, we need to develop a program that can do addition of 2 numbers
 
 NUMBER1=50
-NUMBER2=0.5
+NUMBER2=05
 
 #I need to run a commnd inside shell script to add these 2 numbers
 SUM=$((NUMBER1+NUMBER2))
