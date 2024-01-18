614469da4bce9db714ee51ec66b647603b87e0bd
medamshiva20 <medamshiva20@gmail.com>
2024-01-18 21:49:42 +0530

    global varaibles

--- 16-global_varaibles.sh
+++ 16-global_varaibles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-NUM=200 #global variable
+NUM=200 #This is the global variable it will access through the program or shell script
 getNUM(){
-    #NUM=100 #local variable
+    #NUM=100 #This is local variable it will access with in this block of code only
     echo "$NUM - inside function" 
 }
 
