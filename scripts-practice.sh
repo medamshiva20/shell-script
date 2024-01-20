aa14f05c3326b6f69c1813bf10c8b70ee95a5421
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 21:36:06 +0530

    scripts practicing

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,38 +1,6 @@
 #!/bin/bash 
 
-USER_ID=$(id -u)
-DATE=$(date +%F)
-SCRIPT_NAME=$0
-LOG_FILE=/tmp/$SCRIPT_NAME-$DATE
+VAR1=$1
+VAR2=$2
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-VALIDATE(){
-
-if [ $? -ne 0 ]
- then
-  echo -e "$2 ...$R FAILURE $N"
-  exit 1
- else
-  echo -e "$2 ... $G SUCCESS $N"
-fi
-
-}
-
-if [ $USER_ID -ne 0 ]
- then
-   echo "ERROR: Please run script with root access"
-   exit 126
- else
-   echo "INFO: Script is running with root access"
-fi
-
-
-
-yum install mysql -y >>$LOG_FILE
-VALIDATE $? "Installation of mysql"
-
-yum install postfix -y >>$LOG_FILE
-VALIDATE $? "Installation of postfix"
+echo "variable i is: $VAR1"
