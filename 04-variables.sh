c150d506472e66673a1e61aee082abcd2d3c592c
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 19:35:56 +0530

    global varaibles

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,6 @@ NUMBER1=$1
 NUMBER2=$2
 
 #I need to run a commnd inside shell script to add these 2 numbers
-SUM=$((NUMBER1+NUMBER2))
+SUM=$(NUMBER1+NUMBER2)
 
 echo "Addition of two numbers is: $SUM"
