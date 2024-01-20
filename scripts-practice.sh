a2c9c007a7f2b36aa6083694d721f20e772773aa
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 21:42:07 +0530

    scripts practicing

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
 
-VAR1=$1
-VAR2=$2
-VAR3=$3
-
-echo "variable 1 is: $VAR1"
-echo "script name is: $0"
-echo "All variables are: $@"
-echo "Number of variables: $#"
+for i in {1..100}
+do 
+ echo $i 
+done
