028bd851a9bf0f41753c88befeb46f3852ed84b7
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 22:39:10 +0530

    scripts practicing

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-USERD_ID=$(id -a)
+USER_ID=$(id -a)
 LOG_DIR=/home/centos/shell-script/scriptlog
 SCRIPT_NAME=$0
 DATE=$(date +%F)
@@ -10,7 +10,16 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERD_ID -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
+     then
+      echo -e "$2 ... $R FAILURE $N"
+     else
+      echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $USER_ID -ne 0 ]
  then
    echo "ERROR: Please script with root access"
    exit 1
@@ -20,5 +29,14 @@ fi
 
 for i in $@
 do
- yum install $i -y
+yum list installed $i 
+if [ $? -ne 0 ]
+ then
+  echo "$i packages are not installed let's do install"
+  yum install $i -y
+ else
+   echo -e "$Y ... $i package is already installed"
+fi
 done
+
+VALIDATE $? "$i is installed"
