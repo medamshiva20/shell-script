26c2399f961ea136f6af2cbf75b48a0a847216b1
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 20:37:19 +0530

    scripts practicing

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -3,8 +3,6 @@ USER_ID=$(id -u)
 if [ $USER_ID -nt 0 ]
 then
     echo "ERROR: This script please run with root access or sudo access"
-else
-    echo "INFO: Script is running with root access"
 fi
 
 
