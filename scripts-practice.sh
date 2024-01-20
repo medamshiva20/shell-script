75777ab8d03de81a9194609ff82083fb8601af61
medamshiva20 <medamshiva20@gmail.com>
2024-01-20 20:59:12 +0530

    scripts practicing

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,28 +1,25 @@
-USER_ID=$(id -u)
+#!/bin/bash 
+
+USERD_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]
-then
-    echo "ERROR: This script please run with root access or sudo access"
-    exit 1
+ then
+   echo "ERROR: Please run script with root access"
+ else
+   echo "INFO: Script is running with root access"
 fi
 
+VALIDATE()
+if [ $? -ne 0]
+ then
+  echo "$1 ... $2 FAILURE"
+  exit 1
+ else
+  echo "$1 ... $2 SUCCESS"
+fi
 
 yum install mysql -y
+VALIDATE $? "Installation of mysql"
 
-if [ $? -ne 0 ]
- then
-   echo "FAILURE ... Installation of mysql"
-   exit 10
- else
-   echo "SUCCESS ... Installation of mysql"
-fi
-
-yum install nginx -y
-
-if [ $? -ne 0 ]
- then
-   echo "FAILURE ... Installation of nginx"
-   exit 127
- else
-   echo "SUCCESS ... Installation of nginx"
-fi
+yum install postfix -y
+VALIDATE $? "Installation of postfix"
