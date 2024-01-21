1153b93f46535030a675b776c086729c3d8db2a2
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 16:20:48 +0530

    shell script practice

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -2,12 +2,12 @@
 
 APP_LOGS_DIR=/home/centos/shell-script/app-logs/
 DATE=$(date +%F:%H:%M:%S)
-LOG_DIR=/home/centos/shell-script/shellscript-logs/
+LOG_DIR=/home/centos/shell-script/shellscript-logs
 SCRIPT_NAME=$0
 LOG_FILE=$LOG_DIR/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 echo "Script is executing at: $DATE" &>>$LOG_FILE
-while read
+while read line
 do
  rm -rf $line
  echo "Deleting logs $line" &>>$LOG_FILE
