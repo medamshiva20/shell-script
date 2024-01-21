57c11d06d7133fb5292d5e7f554e4c850d0b8f33
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 16:05:28 +0530

    shell script practice

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,35 +1,13 @@
 #!/bin/bash 
-USERID=$(id -u)
-DATE=$(date +%F)
+
+APP_LOGS_DIR=/home/centos/shell-script/app-logs/
+DATE=$(date +%F:%H:%M:%S)
+LOG_DIR=/home/centos/shell-script/shellscript-logs/
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+LOG_FILE=$LOG_DIR-$SCRIPT_NAME-$DATE
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-#This function should validate the previous command status and inform to user it is success or failure
-VALIDATE(){
- #$1 --> It will receive the argument1
- if [ $? -ne 0 ] # Here we should use $1 or $? it will give same result
-  then 
-      echo -e "Installation of Mysql ...$R FAILURE $N"
-      exit 1
-  else
-      echo -e "Installation of postfix ...$G SUCCESS $N"
- fi
-}
-
-if [ $USERID -ne 0 ]
-  then
-       echo "ERROR:: Please run this script with root access"
-       exit 1
-  else
-       echo "INFO: you are root user"
-fi
-
-yum install mysql -y &>>$LOGFILE
-VALIDATE 
-
-yum install postfix -y &>>$LOGFILE
-VALIDATE  
+while read line
+do
+ rm -rf $line
+ echo "Deleting logs $line"
+done 
