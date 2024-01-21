1047c625435aa229673d4903165abeed2a335c87
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 17:15:09 +0530

    shell script practice

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -5,6 +5,7 @@ LOGFILE_DIRECTORY=/TMP
 SCRIPT_NAME=$0
 DATE=$(date +%F)
 LOG_FILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+message=""
 
 R="\e[31m"
 G="\e[32m"
@@ -18,6 +19,10 @@ while IFS= read line
 do 
   usage=$(echo $line | awk {'print $5'} | cut -d % -f1)
   partition=$(echo $line | awk {'print $1'})
-  echo "Usage is : $usage"
-  echo "Partition is : $partition"
+  #echo "Usage is : $usage"
+  #echo "Partition is : $partition"
+  if [ $usage -gt $DISK_USAGE_THRESHOLD ]
+   then
+     message+="HIGH DISK USAGE ON $partition : $usage\n"
 done <<< $DISK_USAGE
+
