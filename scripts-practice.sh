6b411ca983e189df4cd4ce11330763d2f639d3b5
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 16:09:17 +0530

    shell script practice

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -5,9 +5,9 @@ DATE=$(date +%F:%H:%M:%S)
 LOG_DIR=/home/centos/shell-script/shellscript-logs/
 SCRIPT_NAME=$0
 LOG_FILE=$LOG_DIR-$SCRIPT_NAME-$DATE
-
+FILES_TO_DELETE=$(find * -name "*.log" -type f -mtime +14)
 while read line
 do
  rm -rf $line
  echo "Deleting logs $line"
-done 
+done <<< $FILES_TO_DELETE
