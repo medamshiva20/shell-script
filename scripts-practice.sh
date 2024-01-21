bc76e867bc7c2c42cc50c5b498dedbfcc20bfc47
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 23:17:29 +0530

    shell script

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -7,7 +7,7 @@ SECURITY_GROUP_ID=sg-005b676c6b9725e11
 
 for i in "${NAMES[@]}"
 do
-  if [ $i == "mongodb" || $i == "mysql" ]
+  if [[ $i == "mongodb" || $i == "mysql" ]]
    then
      INSTANCE_TYPE="t3.medium"
    else
