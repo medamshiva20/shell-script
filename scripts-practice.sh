fc3dc0bb95d2c8f0dd51a320282e48ca383deedc
medamshiva20 <medamshiva20@gmail.com>
2024-01-21 23:42:44 +0530

    shell script

--- scripts-practice.sh
+++ scripts-practice.sh
@@ -1,32 +1,54 @@
-#!/bin/bash 
+#!/bin/bash
 
+instance_name=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")  # Replace with the actual name of your instance
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-AMI_ID=ami-0f3c7d07486cad139
-SECURITY_GROUP_ID=sg-005b676c6b9725e11
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-001887f7841106c09
 DOMAIN_NAME=sivadevops.website
 
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
 for i in "${NAMES[@]}"
-do
-  if [[ $i == "mongodb" || $i == "mysql" ]]
-   then
-     INSTANCE_TYPE="t3.medium"
-   else
-     INSTANCE_TYPE="t2.micro"
-  fi
-  echo "creating $i instance"
-  IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-  echo "Instance $i created:$IP_ADDRESS"
-aws route53 change-resource-record-sets --hosted-zone-id Z02176093W0IKU2YA447H --change-batch '
+do 
+
+
+# Check if instance with given name exists
+instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name" --query "Reservations[*].Instances[*].InstanceId" --output text)
+
+    if [ -n "$instance_id" ]; 
+    then
+    echo "Instance with Name '$instance_name' exists.Instance ID: $instance_id"
+    else
+    echo "Instance with Name '$instance_name' does not exist, Let's creating"
+    fi
+
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+
+#https://repost.aws/knowledge-center/simple-resource-record-route53-cli -- To create the route 53 records through AWS CLI 
+    aws route53 change-resource-record-sets --hosted-zone-id Z02130291L9UW40NZZ0BT --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
                             "Name": "'$i.$DOMAIN_NAME'",
                             "Type": "A",
-                            "TTL": 1,
+                            "TTL": 300,
                             "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
                         }}]
     }
     '
 done
+
+# imporvement
+# check instance is already created or not
+# update route53 record
