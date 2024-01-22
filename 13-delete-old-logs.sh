e862e14b68a7a34932c24299cdc0620b93d4cba4
medamshiva20 <medamshiva20@gmail.com>
2024-01-22 17:24:21 +0530

    log files delete

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
 
 log_location=/home/centos/shell-script/app-logs
-noofdays=+2
-cd $log_location
-for zipfile in `find . -name "*.log" -type f -printf "%f\n"`
-do
-  gzip $zipfile
-  wait $!
-done
+# noofdays=+2
+# cd $log_location
+# for zipfile in `find . -name "*.log" -type f -printf "%f\n"`
+# do
+#   gzip $zipfile
+#   wait $!
+# done
 DATE=$(date +%F:%H:%M:%S)
 LOGS_DIR=/home/centos/shell-script/shellscript-logs
 SCRIPT_NAME=$0
 LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
-FILES_TO_DELETE=$(find $log_location -name "*.log.gz" -type f -mtime +14)
+FILES_TO_DELETE=$(find $log_location -name "*.log.gz" -type f -mtime +2)
 
 echo "Script is executing at $DATE" &>>$LOG_FILE
 while read line 
