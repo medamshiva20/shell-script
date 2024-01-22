bcc8ff4b4209b095408a357e949b9f747ffe338e
medamshiva20 <medamshiva20@gmail.com>
2024-01-22 14:02:03 +0530

    created ec2 instance script updated with modifications

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,11 +1,19 @@
 #!/bin/bash 
 
-APP_LOGS_DIR=/home/centos/app-logs/
+log_location=/home/centos/app-logs/
+noofdays=+1
+cd $log_location
+for zipfile in `find . -type f \(-name "log_" -o -name "log-" -name "log." \) printf "%f\n"`
+do
+  gzip $zipfile
+  wait $!
+done
 DATE=$(date +%F:%H:%M:%S)
 LOGS_DIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
 LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $log_location -name "*.log" -type f -mtime +14)
+
 echo "Script is executing at $DATE" &>>$LOG_FILE
 while read line 
 do
