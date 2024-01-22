c181e367c0b06c9119090deff69eec31fb5e4178
medamshiva20 <medamshiva20@gmail.com>
2024-01-22 14:13:27 +0530

    log files zipping

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
 
 log_location=/home/centos/shell-script/app-logs
-noofdays=+1
+noofdays=+2
 cd $log_location
 for zipfile in `find . -name "*.log" -type f -printf "%f\n"`
 do
-  gunzip $zipfile
+  gzip $zipfile
   wait $!
 done
 DATE=$(date +%F:%H:%M:%S)
 LOGS_DIR=/home/centos/shell-script/shellscript-logs
 SCRIPT_NAME=$0
 LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
-FILES_TO_DELETE=$(find $log_location -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $log_location -name "*.log.gz" -type f -mtime +14)
 
 echo "Script is executing at $DATE" &>>$LOG_FILE
 while read line 
