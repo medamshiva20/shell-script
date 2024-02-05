dbff267bf185fc275166259f4cbcf189058a2d53
medamshiva20 <medamshiva20@gmail.com>
2024-02-05 12:55:42 +0530

    Hello World

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -8,3 +8,7 @@ echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good, how are you $PERSON1"
+
+unset $PERSON1
+
+echo "Hello testing variable1 whether we accessing or not:$PERSON1"
