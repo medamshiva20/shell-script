e6a04af6b8589602d384ad82a54eb75b3da29707
medamshiva20 <medamshiva20@gmail.com>
2024-02-07 12:09:33 +0530

    delete-old-logs

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -12,7 +12,7 @@ DATE=$(date +%F:%H:%M:%S)
 LOGS_DIR=/home/centos/shell-script/shellscript-logs
 SCRIPT_NAME=$0
 LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
-FILES_TO_DELETE=$(find $log_location -name "*.log.gz" -type f -mtime +2)
+FILES_TO_DELETE=$(find $log_location -name "*.log" -type f -mtime +14)
 
 echo "Script is executing at $DATE" &>>$LOG_FILE
 while read line 
