f9cd266547f40363c6560b2a58bfcb01b971d4a0
medamshiva20 <medamshiva20@gmail.com>
2024-02-09 14:09:31 +0530

    shell scripts

--- example.sh
+++ example.sh
@@ -8,5 +8,5 @@ variable1=$1
 
 echo "String name is: $variable1"
 echo "Stirng count is: ${#variable1}"
-echo "$variable1" | awk '{print toupper}'
+echo "$variable1" | awk '{print toupper ($0)}'
 echo "$variable1" | awk '{print tolower ($0)}'
